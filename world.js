59fbd987869ed246877bfc145d4b0c025e9e191b
Mubelotix <mubelotix@gmail.com>
2024-12-05 20:07:17 +0100

    Switch to heightmap

--- world.js
+++ world.js
@@ -1,9 +1,13 @@
-export function drawWorld(graphics, depthMap) {
-    for (let x = 0; x < depthMap.length; x++) {
+export function drawWorld(graphics, heightmap) {
+    for (let x = 0; x < heightmap.length; x++) {
         // draw a blue rectangle from x*10 to x*10+10, height is depth_map[x]
-        graphics.rect(x * 10, 0, 10, depthMap[x]*10);
-        graphics.fill(0x0000ff);
 
-        console.log(x * 10, 0, 10, depthMap[x]*10);
+        if (heightmap[x] <= 0) {
+            graphics.rect(x * 20, 0, 20, -heightmap[x]*20);
+            graphics.fill(0x0000ff);
+        }
+
+
+        console.log(x * 20, 0, 20, heightmap[x]*20);
     }
 }
