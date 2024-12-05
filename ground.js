9c0ef658e239b745325e2d243f6e6e87edc88196
Mubelotix <mubelotix@gmail.com>
2024-12-05 22:58:58 +0100

    Start rocks but it's not good

--- ground.js
+++ ground.js
@@ -5,6 +5,23 @@ export async function initKelp(screen) {
     const kelpTexture2 = await PIXI.Assets.load('/assets/kelp2.png');
     const coralTexture = await PIXI.Assets.load('/assets/corail1.png');
     const coralTexture2 = await PIXI.Assets.load('/assets/corail2.png');
+    // const rockTexture = await PIXI.Assets.load('/assets/rock1.png');
+    // const rockTexture2 = await PIXI.Assets.load('/assets/rock2.png');
+
+    // for (let i = 0; i < heightmap.length; i++) {
+    //     if (Math.random() < 0.9) {
+    //         continue;
+    //     }
+        
+    //     let randomTexture = Math.random() > 0.5 ? rockTexture : rockTexture2;
+    //     const rock = new PIXI.Sprite(randomTexture);
+    //     rock.width = 120;
+    //     rock.height = 120;
+    //     rock.x = i * 20 - 60;
+    //     rock.y = -heightmap[i] * 20 - 120;
+    //     rock.alpha = 0.9;
+    //     screen.addChild(rock);
+    // }
 
     for (let i = 0; i < heightmap.length; i++) {
         let randomTexture;
