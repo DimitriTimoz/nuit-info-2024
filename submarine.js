b1abe6fafdca13450d9047d97ceb52404bb989b2
Dimitri <dimitri.timoz@orange.fr>
2024-12-05 20:47:11 +0100

    movign

--- submarine.js
+++ submarine.js
@@ -1,14 +1,15 @@
 export class Submarine {
-    constructor(sprite, screen) {
+    constructor(app, screen, sprite) {
         this.sprite = sprite;
         this.screen = screen;
         this.speed = 5;
+        this.app = app;
         this.set_sprite_middle();
     }
     set_sprite_middle() {
         this.sprite.anchor.set(0.5, 0.5);
-        this.sprite.x = this.screen.x;
-        this.sprite.y = this.screen.y;
+        this.sprite.x = this.app.screen.width / 2;
+        this.sprite.y = this.app.screen.height / 2;
     }
     move(direction) {
         switch (direction) {
@@ -24,7 +25,7 @@ export class Submarine {
                 break;
             case 'right':
                 this.screen.x -= this.speed;
-                this.sprite.scale.x = 1;
+                this.sprite.scale.x = 1;   
                 break;
         }
     }
