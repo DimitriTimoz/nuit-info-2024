1e566af0817dfce5078761ceca21f7f13b1431e6
Dimitri <dimitri.timoz@orange.fr>
2024-12-05 20:40:03 +0100

    sub

--- submarine.js
+++ submarine.js
@@ -3,8 +3,13 @@ export class Submarine {
         this.sprite = sprite;
         this.screen = screen;
         this.speed = 5;
+        this.set_sprite_middle();
     }
-
+    set_sprite_middle() {
+        this.sprite.anchor.set(0.5, 0.5);
+        this.sprite.x = this.screen.x;
+        this.sprite.y = this.screen.y;
+    }
     move(direction) {
         switch (direction) {
             case 'up':
@@ -15,9 +20,11 @@ export class Submarine {
                 break;
             case 'left':
                 this.screen.x += this.speed;
+                this.sprite.scale.x = -1;
                 break;
             case 'right':
                 this.screen.x -= this.speed;
+                this.sprite.scale.x = 1;
                 break;
         }
     }
