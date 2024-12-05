67c4246dfef2aab03f0a553875aa3bc42315987a
Mubelotix <mubelotix@gmail.com>
2024-12-06 00:03:21 +0100

    Add ground coloring

--- world.js
+++ world.js
@@ -1,12 +1,19 @@
-export function drawWorld(graphics, heightmap) {
+export function drawWorld(screen, heightmap) {
+    // Sea
+    const graphics = new PIXI.Graphics();
     for (let x = 0; x < heightmap.length; x++) {
-        // draw a blue rectangle from x*10 to x*10+10, height is depth_map[x]
-
         if (heightmap[x] <= 0) {
             graphics.rect(x * 20, 0, 20, -heightmap[x]*20);
-            graphics.fill(0x0000ff);
         }
-
-
     }
+    graphics.fill(0x0000ff);
+    screen.addChild(graphics);
+
+    // Ground
+    const graphics2 = new PIXI.Graphics();
+    for (let x = 0; x < heightmap.length; x++) {
+        graphics2.rect(x * 20, -heightmap[x]*20, 20, 1000000);
+    }
+    graphics2.fill(0x1a0f00);
+    screen.addChild(graphics2);
 }
