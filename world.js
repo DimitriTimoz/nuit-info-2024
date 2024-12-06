14a20d6f4615da1534b2d2d9cfb5cb1da0bd0229
Dimitri <dimitri.timoz@orange.fr>
2024-12-06 05:49:47 +0100

    improve style

--- world.js
+++ world.js
@@ -1,19 +1,37 @@
 export function drawWorld(screen, heightmap) {
-    // Sea
-    const graphics = new PIXI.Graphics();
+    // Sky gradient
+    const skyGradient = new PIXI.Graphics();
+    skyGradient.beginFill(0x87CEEB)
+        .drawRect(0, -1000, heightmap.length * 20, 1000)
+        .endFill();
+    screen.addChild(skyGradient);
+
+    // Sea with gradient
+    const seaGraphics = new PIXI.Graphics();
+    const gradient = new PIXI.Graphics();
+    gradient.beginFill(0x006994, 0.6)
+        .drawRect(0, 0, heightmap.length * 20, 1000)
+        .endFill();
+    
     for (let x = 0; x < heightmap.length; x++) {
         if (heightmap[x] <= 0) {
-            graphics.rect(x * 20, 0, 20, -heightmap[x]*20);
+            seaGraphics.beginFill(0x0066cc, 0.4)
+                .drawRect(x * 20, 0, 20, -heightmap[x]*20)
+                .endFill();
         }
     }
-    graphics.fill(0x0000ff);
-    screen.addChild(graphics);
+    
+    screen.addChild(seaGraphics);
+    screen.addChild(gradient);
 
-    // Ground
-    const graphics2 = new PIXI.Graphics();
+    // Ground with gradient
+    const groundGraphics = new PIXI.Graphics();
     for (let x = 0; x < heightmap.length; x++) {
-        graphics2.rect(x * 20, -heightmap[x]*20, 20, 1000000);
+        const groundColor = heightmap[x] <= 0 ? 0x3d2b1f : 0x355e3b;
+        const alpha = heightmap[x] <= 0 ? 1 : 0.9;
+        groundGraphics.beginFill(groundColor, alpha)
+            .drawRect(x * 20, -heightmap[x]*20, 20, 1000000)
+            .endFill();
     }
-    graphics2.fill(0x1a0f00);
-    screen.addChild(graphics2);
+    screen.addChild(groundGraphics);
 }
