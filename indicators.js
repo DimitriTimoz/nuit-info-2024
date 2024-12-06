fa17b92a933b81b115b15d90d1132d3d1470b66e
Mubelotix <mubelotix@gmail.com>
2024-12-06 03:18:08 +0100

    shit

--- indicators.js
+++ indicators.js
@@ -1,4 +1,11 @@
 export async function initIndicators(app, screen) {
+    let basicText = new PIXI.Text({ text: 'Basic text in pixi' });
+    basicText.x = app.screen.width / 2;
+    basicText.y = 140;
+    basicText.anchor.set(0.5);
+    basicText.alpha = 0;
+    app.stage.addChild(basicText);
+
     let earTexture = await PIXI.Assets.load('assets/ear.png');
     let ear = new PIXI.Sprite(earTexture);
     ear.anchor.set(0.5);
@@ -6,6 +13,14 @@ export async function initIndicators(app, screen) {
     ear.y = 110;
     ear.scale.x *= 0.05;
     ear.scale.y *= 0.05;
+    ear.eventMode = 'static';
+    ear.on('mouseover', () => {
+        console.log('mouseover');
+        basicText.alpha = 1;
+    });
+    ear.on('mouseout', () => {
+        basicText.alpha = 0;
+    });
     app.stage.addChild(ear);
 
     app.ticker.add((delta) => {
@@ -15,7 +30,7 @@ export async function initIndicators(app, screen) {
                 croisiere_counts++;
             }
         }
-        // ear.alpha = croisiere_counts > 3 ? 1 : 0;
+        ear.alpha = croisiere_counts > 3 ? Math.sin(delta.lastTime * 0.007) : 0;
     });
 }
 
